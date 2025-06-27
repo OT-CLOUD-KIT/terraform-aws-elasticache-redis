a5ec288b2d4366aaf2c4dc5ab70ef6d65ee51aa8
jnikita19 <Nikita.Joshi.snaatak@mygurukulam.co>
2025-06-27 08:04:16 +0530

    update in variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,23 @@
 # variable "cluster_id" {}
 variable "engine" {
   default = "redis"
+
+
 }
-variable "node_type" {}
+variable "node_type" {
+  default = "cache.t3.micro"
+}
 variable "port" {
   default = 6379
 }
 
-variable "vpc_id" {}
+variable "vpc_id" {
+  default = "vpc-0bfa15004ff55e107"
+}
 variable "subnet_ids" {
   type = list(string)
+  default = [ "subnet-08a2aa30dbc179a2b" , "subnet-0a49bf4221b5f0107" ]
 }
-# variable "subnet_group_name" {}
 
 variable "create_default_security_group" {
   type    = bool
@@ -19,9 +25,11 @@ variable "create_default_security_group" {
 }
 variable "allowed_ingress_ports" {
   type = list(number)
+  default = [ 6379 ]
 }
 variable "allowed_ingress_cidr_blocks" {
   type = list(string)
+  default = [ "10.0.0.0/16" ]
 }
 
 variable "parameter_group_enabled" {
@@ -34,6 +42,7 @@ variable "parameter_group_name" {
 }
 variable "redis_family" {
   type = string
+  default = "redis7"
 }
 variable "parameter" {
   type    = list(object({ name = string, value = string }))
@@ -45,10 +54,7 @@ variable "security_group_ids" {
   default = []
 }
 
-# variable "tags" {
-#   type    = map(string)
-#   default = {}
-# }
+
 
 
 ################################### Naming convention variables #########################################
@@ -56,6 +62,7 @@ variable "security_group_ids" {
 variable "bu" {
   description = "Business unit name (e.g., BP, GURUKU). Max 6 characters."
   type        = string
+  default = "bp"
 
   validation {
     condition     = length(var.bu) <= 10
@@ -66,11 +73,13 @@ variable "bu" {
 variable "program" {
   description = "Name of the program (e.g., OT, BP)."
   type        = string
+  default = "ot"
 }
 
 variable "app" {
   description = "Application name (e.g., network, shared). Max 6 characters."
   type        = string
+
 
   validation {
     condition     = length(var.app) <= 10
@@ -81,6 +90,7 @@ variable "app" {
 variable "env" {
   description = "Environment code: 'd' (dev), 'p' (prod), 'q' (qa), 's' (stage), 'g' (global)."
   type        = string
+  default = "d"
 
   validation {
     condition     = contains(["d", "p", "q", "s", "g"], var.env)
@@ -91,9 +101,11 @@ variable "env" {
 variable "team" {
   description = "Team email responsible for the application (e.g., digitalops@gehealthcare.com)."
   type        = string
+  default = "infra"
 }
 
 variable "region" {
   description = "AWS region (e.g., us-east-1, ap-south-1)."
   type        = string
-}
+  default = "us-east-1"
+} 
